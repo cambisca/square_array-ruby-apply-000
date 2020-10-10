e573a15ad7ee087e3afd66f0f9088e1eda8effff
Cameron Bisca <cjbisca@gmail.com>
2020-10-10 23:04:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,4 +6,4 @@ def square_array(array)
 end
 
 #A Note on Return Values
-Different iterators have different return values. Notice that the return value of the call to #each above returned ["Tim", "Tom", "Jim"] – the original array. The #each method will always return the original collection on which it was called.
+#Different iterators have different return values. Notice that the return value of the call to #each above returned ["Tim", "Tom", "Jim"] – the #original array. The #each method will always return the original collection on which it was called.
