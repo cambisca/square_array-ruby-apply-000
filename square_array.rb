e79d4bb34d63dba50299dc3d04bc6a14b02ff6a3
Cameron Bisca <cjbisca@gmail.com>
2020-10-10 23:23:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
 array = [1, 2, 3]
 
 def square_array(array)
-  new_array == array.each do |number| 
+  array.each do |number| 
       number ** 2 
   end  
-  puts new_array
 end
 
 #A Note on Return Values
