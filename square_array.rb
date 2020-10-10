859652fed688a604dad69d49dc0e70c1f164958a
Cameron Bisca <cjbisca@gmail.com>
2020-10-10 23:26:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
 array = [1, 2, 3]
 
 def square_array(array)
+  counter = 0
   array.each do |number| 
-      number ** 2 
-        return array 
+      number ** 2 == array[0]
+        counter += 1
   end  
 end
 
