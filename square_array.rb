6b95f5435c07e48eea5f301722b1383191bce677
Cameron Bisca <cjbisca@gmail.com>
2020-10-10 23:03:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,3 +4,6 @@ def square_array(array)
   array.each do |number| 
     
 end
+
+#A Note on Return Values
+Different iterators have different return values. Notice that the return value of the call to #each above returned ["Tim", "Tom", "Jim"] – the original array. The #each method will always return the original collection on which it was called.
